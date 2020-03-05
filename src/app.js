92341343addc9cd24e80986a99e94996bbfd2bdf
besimgurbuz <mbesimgurbuz@gmail.com>
2020-03-05 23:53:52 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const problems = require('./problems.json');
+
+const random = Math.floor(Math.random() * problems.length);
+const randomProblem = problems[random];
+
+const content = `Your problem is below, may the force be with you padawan!
+- Problem Name: ${randomProblem.name}
+
+${randomProblem.description}
+
+This problems rank is ${randomProblem.rank}
+And do not forget JavaScript is the best!
+- Besim
+`
+
+fs.writeFile('your_problem_dear_padawan.txt', content, (err) => {
+  if (err) {
+    throw err;    
+  }
+  console.log('Your problem is ready to solve! Check your current working directory. 📁');
+});
